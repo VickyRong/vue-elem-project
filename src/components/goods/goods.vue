446fdf0ee4401a64cf9f5ebbcdc50b6e3463f949
VickyRong <huyanrong@yrdeMacBook-Pro.local>
2017-02-10 15:33:42 +0800

    菜单滚动映射完成
    滚动右边的菜单，左边的导航可以跟随滚动，建立映射。

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -3,7 +3,7 @@
     <!--左侧导航-->
     <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="item in goods" class="menu-item" :class="{'current':currentIndex === $index}">
           <span class="text">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
@@ -14,7 +14,7 @@
     <!--右侧菜品-->
     <div class="foods-wrapper" v-el:foods-wrapper>
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -29,8 +29,7 @@
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span>
-                  <span v-show="oldPrice" class="old">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -56,8 +55,22 @@
     },
     data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       };
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+              return i;
+          }
+        }
+        return 0;
+      }
     },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -67,14 +80,32 @@
           this.goods = response.data;
           this.$nextTick(() => {
             this._initScroll();
+            this._calculateHeight();
           });
         }
       });
     },
     methods: {
-      _initScroll: function ($event) {
+      _initScroll: function () {  // 初始化滚动条插件
         this.menuScroll = new BScroll(this.$els.menuWrapper, {});
-        this.foodsScroll = new BScroll(this.$els.foodsWrapper, {});
+        this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+          probeType: 3
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+          console.log(this.scrollY);
+        });
+      },
+      _calculateHeight: function () { // 计算滚动时页面高度
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height); // 得到每个foodList的高度值，并存在数组里面
+        }
+        console.log(this.listHeight);
       }
     }
   };
