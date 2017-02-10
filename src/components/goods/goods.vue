6a3bc09342da864bb29163df349ef493f262b63c
VickyRong <huyanrong@yrdeMacBook-Pro.local>
2017-02-10 10:42:52 +0800

    添加菜单导航和食物列表
    运用页面滚动插件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,7 +1,43 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper"></div>
-    <div class="foods-wrapper"></div>
+    <!--左侧导航-->
+    <div class="menu-wrapper" v-el:menu-wrapper>
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <!--右侧菜品-->
+    <div class="foods-wrapper" v-el:foods-wrapper>
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon">
+                <img :src="food.icon" width="57" height="57">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span v-show="oldPrice" class="old">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <style lang="stylus" rel="stylesheet/stylus">
@@ -9,6 +45,8 @@
   @import "goods.styl";
 </style>
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+
   const ERR_OK = 0;
   export default {
     props: {
@@ -22,13 +60,22 @@
       };
     },
     created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
       this.$http.get('/api/goods').then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
-          console.log(this.goods);
+          this.$nextTick(() => {
+            this._initScroll();
+          });
         }
       });
+    },
+    methods: {
+      _initScroll: function ($event) {
+        this.menuScroll = new BScroll(this.$els.menuWrapper, {});
+        this.foodsScroll = new BScroll(this.$els.foodsWrapper, {});
+      }
     }
   };
 </script>
